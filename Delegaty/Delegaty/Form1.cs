df8192e9fe2075d0274eef7afca486288a3823de
olivierdermount <olivierdermount@gmail.com>
2021-01-14 13:41:43 +0100

    Wstawiono Nowe Repozytorium

---
+++ Delegaty/Delegaty/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Delegaty
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void SetColumnHeader()
+        {
+            //Ciało metody
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("MousEnter");
+        }
+    }
+}
