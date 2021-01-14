5d6a8e10ff2fe08647438483f3b2f2ab4918b9f0
olivierdermount <olivierdermount@gmail.com>
2021-01-14 14:03:35 +0100

    Dodano przycisk

--- Delegaty/Delegaty/Form1.Designer.cs
+++ Delegaty/Delegaty/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace Delegaty
         private void InitializeComponent()
         {
             this.btnAdd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnAdd
@@ -43,11 +44,21 @@ namespace Delegaty
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             this.btnAdd.MouseEnter += new System.EventHandler(this.btnAdd_MouseEnter);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Odejmij";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnAdd);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,6 +69,7 @@ namespace Delegaty
         #endregion
 
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button button1;
     }
 }
 
